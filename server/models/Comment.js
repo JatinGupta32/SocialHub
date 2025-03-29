d2207eb42bd35504495bbcb4992ae0123379367d
JatinGupta32 <jatinji890@gmail.com>
2025-03-29 12:49:49 +0530

    ggggggg

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -17,4 +17,3 @@ const commentSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model("comment",commentSchema);
-//fedfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff

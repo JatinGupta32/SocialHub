42c0ad00e5f5e20a4745e8140a914ce80f68e67f
JatinGupta32 <jatinji890@gmail.com>
2025-04-20 22:45:51 +0530

    checking

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 app.use(
     cors({
         // origin: "http://localhost:5173",
-        origin: "https://socialhub-gamma.vercel.app/",
+        origin: "https://socialhub-gamma.vercel.app",
         credentials: true,
     })
 );
